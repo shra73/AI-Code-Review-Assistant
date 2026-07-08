b29fcd16890e42e78310e4a02436ac517188501d
shra73 <shra94270@gmail.com>
2026-07-08 15:07:39 +0530

    Day 2: Added hero section to homepage

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to AI Code Review Assistant</h1>"
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
